afe5484968475ffdd7aee0c0a16004cdfab39a97
Marc S. Brooks <marc@mbrooks.info>
2022-09-21 23:59:57 -0700

    Removed build testing dependencies
    Removed redundant docker command

--- user-data.sh
+++ user-data.sh
@@ -10,17 +10,12 @@
 #   - This script has been tested to work with RHEL & CentOS
 #   - This script must be run as root
 
-[ -d /root/.build ] && exit 0
-
 # Install dependencies.
 yum -y install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-yum -y install git
 
 amazon-linux-extras install docker
 
 service docker start && chkconfig docker on
-
-git clone --recurse-submodules https://github.com/nuxy/aws-ec2-opensimulator.git /root/.build
 
 # Create 4GB swapfile (support t2.small)
 dd if=/dev/zero of=/swapfile bs=128M count=32
@@ -33,6 +28,4 @@ if [ -f /swapfile ]; then
 fi
 
 # Launch the grid server.
-docker pull marcsbrooks/docker-opensimulator-server
-
 docker run -d --network host --restart always marcsbrooks/docker-opensimulator-server:latest
